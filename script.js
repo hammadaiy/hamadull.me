a4de6cfee9c22b7536877c3b800b5f3dc36efae7
Hamad Ullah <hamadull@uef.fi>
2024-11-12 18:15:56 +0200

    github repos added to the side container

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ function toggleMenu() {
   links.classList.toggle("show");
   hamburger.classList.toggle("open");
 }
+
+//Github Repositories
+const languageColors = {
+  JavaScript: "#f1e05a",
+  Python: "#3572A5",
+  Java: "#b07219",
+  HTML: "#e34c26",
+  CSS: "#563d7c",
+};
+
+fetch("https://api.github.com/users/hammadaiy/repos")
+  .then((response) => response.json())
+  .then((data) => {
+    const repoList = document.getElementById("repo-list");
+    data.forEach((repo) => {
+      const listItem = document.createElement("li");
+      if (repo.language) {
+        const languageColor = languageColors[repo.language] || "#000000";
+        listItem.innerHTML = `
+                      <a href="${repo.html_url}">${repo.name}</a> - 
+                      <span style="color: ${languageColor};">&#9679;</span> ${repo.language}
+                  `;
+      } else {
+        listItem.innerHTML = `<a href="${repo.html_url}">${repo.name}</a>`;
+      }
+      repoList.appendChild(listItem);
+    });
+  })
+  .catch((error) => console.error("Error fetching repositories:", error));
